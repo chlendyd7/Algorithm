215dd99e0e588278ac2a91d88619edb1b234112f
Choi Du Young <chlendyd7@naver.com>
2022-03-11 23:37:00 +0900

    3/11

--- src/input_output/A_10718.java
+++ src/input_output/A_10718.java
@@ -4,6 +4,8 @@ public class A_10718 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
 
 	}
 
