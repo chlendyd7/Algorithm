6485ea18995240e72d42ce82b588b3173a11add8
Choi Du Young <chlendyd7@naver.com>
2022-03-14 13:50:39 +0900

    3.14

---
+++ src/input_output/A_18108.java
@@ -0,0 +1,13 @@
+package input_output;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class A_18108 {
+
+	public static void main(String[] args) throws IOException{
+		Scanner sc = new Scanner(System.in);
+		System.out.println(sc.nextInt()-543);
+	}
+
+}
