81ee3ded0b8d44fd7b2666dcd64447811749385a
Choi Du Young <chlendyd7@naver.com>
2022-04-03 16:07:04 +0900

    ?

---
+++ Algorithm_BackJoon/bin/For/A_10950/A_10950/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_10950
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = new int[int.Parse(Console.ReadLine())];
+            int[] sum = new int[num.Length];
+
+            for(int i =0; i<num.Length; i++)
+            {
+                string[] plus = Console.ReadLine().Split();
+                sum[i] = int.Parse(plus[0]) + int.Parse(plus[1]);
+            }
+            for(int i=0; i<num.Length; i++)
+                Console.WriteLine(sum[i]);
+
+            Console.Read();
+        }
+    }
+}
