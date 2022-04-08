7a87e2f9517175e181fe545838dc17d00eb2b6c8
Choi Du Young <chlendyd7@naver.com>
2022-04-08 18:08:03 +0900

    A_10818

---
+++ Algorithm_BackJoon/BackJoon/Array1/A_10818.java
@@ -0,0 +1,19 @@
+package Array1;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main{
+	public static void main (String[]args ) {
+		
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] array =new int [n];
+		
+		for (int i=0; i<array.length; i++) {
+			array[i]=sc.nextInt();	
+		}
+		Arrays.sort(array);
+		System.out.println(array[0]+" "+array[n-1]);	
+	}
+}
