6a801c4daa59b1a8f3d39854b85e685ef26a4a3d
Choi Du Young <chlendyd7@naver.com>
2022-04-09 10:04:32 +0900

    A_1110

--- Algorithm_BackJoon/BackJoon/Array1/A_10818.java
+++ Algorithm_BackJoon/BackJoon/Array1/A_10818.java
@@ -3,7 +3,7 @@ package Array1;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main{
+public class A_10818{
 	public static void main (String[]args ) {
 		
 		Scanner sc = new Scanner(System.in);
