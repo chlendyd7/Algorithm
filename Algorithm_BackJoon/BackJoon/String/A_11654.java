ebcec5eb03ed509d16bf11ff051af50b83e9f30b
Choi Du Young <chlendyd7@naver.com>
2022-04-20 23:36:53 +0900

    04_20

---
+++ Algorithm_BackJoon/BackJoon/String/A_11654.java
@@ -0,0 +1,13 @@
+package String;
+
+import java.io.IOException;
+
+public class A_11654 {
+
+	public static void main(String[] args) throws IOException {
+		int a = System.in.read();
+		
+		System.out.print(a);
+	}
+
+}
