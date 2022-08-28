f6f5f19842f15588eac674d6e851fae448737f84
Choi Du Young <chlendyd7@naver.com>
2022-08-28 14:47:19 +0900

    8_28

--- Algorithm_BackJoon/Python/1712.py
+++ Algorithm_BackJoon/Python/1712.py
@@ -1,7 +1,19 @@
-a, b ,c = input().split()
-i = 1
-cnt=0
-while (c*i)<(a+b*i):
-    cnt +=1
-    i +=1
-print(cnt)1234
+# a, b ,c = map(int,input().split())
+# i = 0
+# cnt=0
+# if c < b :
+#     print(-1)
+
+# else:
+#     while (c*i)<=(a+(b*i)):
+#         cnt +=1
+#         i +=1
+
+# print(cnt)
+## Ctrl + K + C 전체 주석
+
+a,b,c = map(int, input().split())
+if c<=b:
+    print(-1)
+else:
+    print(int(a/(c-b)+1))
