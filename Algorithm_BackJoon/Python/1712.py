20364c9c6afb51aeb46659d17aa3dd20bab9e4e4
chlendyd7 <chlendyd7@naver.com>
2022-09-06 11:19:01 +0900

    Update 1712.py

--- Algorithm_BackJoon/Python/1712.py
+++ Algorithm_BackJoon/Python/1712.py
@@ -1,7 +1,11 @@
 a, b ,c = input().split()
 i = 1
 cnt=0
-while (c*i)<(a+b*i):
-    cnt +=1
-    i +=1
-print(cnt)1234
+if b >= c :
+    cnt = -1
+else:
+    while (c*i)<(a+b*i):
+        cnt +=1
+        i +=1
+        print(1)
+print(cnt) n
