37696aab48bfad91065b64660a0321148d439030
Choi Du Young <chlendyd7@naver.com>
2022-09-11 16:07:19 +0900

    1

--- Algorithm_BackJoon/Python/1929.py
+++ Algorithm_BackJoon/Python/1929.py
@@ -1,9 +1,8 @@
 a, b = map(int,input().split())
 
 for i in range(a,b+1):
-    if i > 1:
-        for j in range(2, i):
+    for j in range(2, i):
             if i % j ==0:
                 break
-        else:
+    else:
             print(i)
