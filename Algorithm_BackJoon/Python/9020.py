f844ef1e7499a200a0006c27a2d4193ae579be27
Choi Du Young <chlendyd7@naver.com>
2022-09-11 17:27:47 +0900

    Create 9020.py

---
+++ Algorithm_BackJoon/Python/9020.py
@@ -0,0 +1,20 @@
+def sosu(n):
+    if n == 1:
+        return False
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return False
+    return True
+
+for _ in range(int(input())):
+    num = int(input())
+
+    a,b = num//2, num//2
+    while a > 0:
+        if sosu(a) and sosu(b):
+            print(a, b)
+            break
+        else:
+            a -= 1
+            b += 1
+
