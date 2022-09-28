e59ddc4c96ea1b25fa36abd477e4193e2eb8bf7b
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2022-09-28 19:08:31 +0900

    1

--- inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 3/4. 두 리스트 합치기/AA.py
+++ inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 3/4. 두 리스트 합치기/AA.py
@@ -1,14 +1,19 @@
-a = input()
+a = int(input())
 b = list(map(int,input().split()))
-c = input()
+c = int(input())
 d = list(map(int,input().split()))
 e = []
-
-for i in b:
-    e.append(i)
-for j in d:
-    e.append(j)
-
-e.sort()
-for z in e:
-    print(z, end =" ")
+ai = ci = 0
+while a < ai and c < ci:
+    if b[ai] < d[ci]:
+        e.append(b[ai])
+        ai += 1
+    else:
+        e.append(d[ci])
+        ci +=1
+if ai < a:
+    e = e+d[ai:]
+if ci < c:
+    e = e+d[ci:]
+for x in e:
+    print(x, end='')
