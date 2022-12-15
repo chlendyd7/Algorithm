e98300e5fed5d006a029c8c5c361332d501a238d
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2022-12-15 19:58:58 +0900

    1

--- inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 5/1. 가장 큰 수/AA.py
+++ inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 5/1. 가장 큰 수/AA.py
@@ -1,4 +1,5 @@
 num, m=map(int, input().split())
+<<<<<<< Updated upstream
 num=list(map(int, str(num)))#이렇게 해주는 이유?
 stack=[]
 for x in num:
@@ -14,3 +15,21 @@ print(res)
 # 정리: 첫번째 while문이 돌 때에는 stack이 빈 list라 넘어가게 됨
 # 계속해서 num을 돌려야 하기 때문에 stack[-1]이 < 현재의 list의 값과 비교
 # for 문 다 돌았는데도 m이 남아 있을 시 뒷자리부터 제거하기 때문에 [:-m]으로 슬라이싱
+=======
+num=list(map(int, str(num)))
+stack=[]
+for x in num:
+    while stack and m>0 and stack[-1]<x: #stack의 list안에 값이 있으면 참을 리턴
+        stack.pop()
+        m-=1
+    stack.append(x)
+if m!=0:
+    stack=stack[:-m]
+res=''.join(map(str, stack))
+print(res)
+
+
+
+
+
+>>>>>>> Stashed changes
