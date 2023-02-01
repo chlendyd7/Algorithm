31cc2fe514e0fcb08ca651d6ec6cf5ddf0f911b0
chlendyd7 <chlendyd7@naver.com>
2023-02-01 16:48:45 +0900

    02_01

---
+++ inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 7/12. 단지번호붙이기/answer.py
@@ -0,0 +1,28 @@
+dx=[-1, 0, 1, 0]
+dy=[0, 1, 0, -1]
+
+def DFS(x, y):
+    global cnt
+    cnt+=1 # 왜 cnt +1 검증 안해도 되는가? 하나의 집 좌표가 발견되면 상관없기 때문
+    board[x][y]=0 # 왜 바로 0으로 초기화? 중복방문 막기 위함
+    for i in range(4):
+        xx=x+dx[i]
+        yy=y+dy[i]
+        if 0<=xx<n and 0<=yy<n and board[xx][yy]==1:
+            DFS(xx, yy)
+
+if __name__=="__main__":
+    n=int(input())
+    board=[list(map(int, input())) for _ in range(n)] # 띄어쓰기가 없어 input()으로 한 줄 다 읽음
+    res=[]
+    for i in range(n):
+        for j in range(n):
+            if board[i][j]==1:
+                cnt=0
+                DFS(i, j)
+                res.append(cnt)
+    print(len(res))
+    res.sort()
+    for x in res:
+        print(x)
+
