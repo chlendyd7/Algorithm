7485ef4e0f80c50d128e024afcd412c28a5e434f
chlendyd7 <chlendyd7@naver.com>
2023-02-08 23:07:39 +0900

    02_08

--- inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 7/17. 피자 배달 거리/answer_DFS.py
+++ inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 7/17. 피자 배달 거리/answer_DFS.py
@@ -5,19 +5,19 @@ def DFS(L, s):
     if L==m:
         sum=0
         for j in range(len(hs)):
-            x1=hs[j][0] # 집 튜플에 첫 번째 값
+            x1=hs[j][0] # 집 하나 결정
             y1=hs[j][1]
             dis=2147000000
-            for x in cb:
-                x2=pz[x][0]
+            for x in cb: # m개 for문
+                x2=pz[x][0] 
                 y2=pz[x][1]
                 dis=min(dis, abs(x1-x2)+abs(y1-y2)) #거리
             sum+=dis
         if sum<res:
             res=sum    
     else:
-        for i in range(s, len(pz)):
-            cb[L]=i
+        for i in range(s, len(pz)): # 조합 구하는 방법 복습 필요
+            cb[L]=i # 0~5까지 돔
             DFS(L+1, i+1)
 
 
