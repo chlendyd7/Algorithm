5465234040fbe22ca6039b43d2fb22663c486cb3
chlendyd7 <chlendyd7@naver.com>
2023-02-13 10:38:21 +0900

    02_13

--- inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 8/5. 최대 선 연결하기/answer.py
+++ inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 8/5. 최대 선 연결하기/answer.py
@@ -1,10 +1,10 @@
 import sys
-# sys.stdin = open("input.txt", 'r')
+sys.stdin = open("input.txt", 'r')
 n=int(input())
 arr=list(map(int, input().split()))
-arr.insert(0,0) # list가 1번 index에서 시작하게 하기 위해서
-dy=[0]*(n+1) # 다이나믹
-dy[1]=1 # 직관적으로 처음 값은 1인것을 안다
+arr.insert(0,0)
+dy=[0]*(n+1)
+dy[1]=1
 res=0
 for i in range(2, n+1):
     max=0
@@ -12,6 +12,6 @@ for i in range(2, n+1):
         if arr[j]<arr[i] and dy[j]>max:
             max=dy[j]
     dy[i]=max+1
-    if dy[i]>res: # 최대값 
+    if dy[i]>res:
         res=dy[i]
 print(res)
