f293e5ba57ed44fa7080bf9995dbd250b35dc84a
Doo Young <88640035+chlendyd7@users.noreply.github.com>
2024-03-13 17:21:50 +0900

    24_03_13

--- inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 5/1. 가장 큰 수/AA.py
+++ inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 5/1. 가장 큰 수/AA.py
@@ -1,12 +1,63 @@
-num, m=map(int, input().split())
-num=list(map(int, str(num)))
+'''
+    선생님은 현수에게 숫자 하나를 주고, 해당 숫자의 자릿수들 중 m개의 숫자를 제거하
+    여 가장 큰 수를 만들라고 했습니다. 여러분이 현수를 도와주세요.(단 숫자의 순서는
+    유지해야 합니다)
+    만약 5276823 이 주어지고 3개의 자릿수를 제거한다면
+    7823이 가장 큰 숫자가 됩니다.
+    1. 빈 stack 만들기
+    2. 넣기
+    3. 비교하기 들어가려는 값이 더 크면
+    4. 빼기(m개)
+    5. m가 남아있으면 m개 만큼 버리기
+    6. ex) 987654 [:-m]
+
+'''
+num,m=map(int,input().split())
+num = list(map(int, str(num)))
 stack=[]
 for x in num:
-    while stack and m>0 and stack[-1]<x: #stack의 list안에 값이 있으면 참을 리턴
+    while stack and m>0 and x>stack[-1]:
         stack.pop()
         m-=1
     stack.append(x)
-if m!=0:
-    stack=stack[:-m]
+
+if m !=0:
+    stack = stack[:-m]
 res=''.join(map(str, stack))
 print(res)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# num, m=map(int, input().split())
+# num=list(map(int, str(num)))
+# stack=[]
+# for x in num:
+#     while stack and m>0 and stack[-1]<x: #stack의 list안에 값이 있으면 참을 리턴
+#         stack.pop()
+#         m-=1
+#     stack.append(x)
+# if m!=0:
+#     stack=stack[:-m]
+# res=''.join(map(str, stack))
+# print(res)
+# '''
+# 7823 
+# '''
--- inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 5/2. 쇠막대기/AA.py
+++ inflearn/파이썬 알고리즘 문제풀이(코딩테스트 대비)/섹션 5/2. 쇠막대기/AA.py
@@ -1,22 +1,15 @@
-s = input()
-stack = []
-res = ''
-for i in s:
-    if s.isdecimal():
-        res += s
-    else:
-        if x == '(':
-            stack.append(s)
+import sys
+sys.stdin=open("input.txt", "r")
+s=input()
+stack=[]
+cnt=0
+for i in range(len(s)):
     if s[i]=='(':
         stack.append(s[i])
-    else: # )가 들어온다면 
+    else:
         stack.pop()
-        if s[i-1]=='(': # 레이저 빔~
-            cnt += len(stack)
-        else: # 앞자리가 )이다
-            cnt += 1
+        if s[i-1]=='(':
+            cnt+=len(stack)
+        else:
+            cnt+=1
 print(cnt)
-
-
-
-# 질문: 쇠막대기는 어디서 표현되는가? ()는 레이저, (((는 쇠막대기 갯수 )시 앞에 (가 없으면 쇠막대기 닫는 괄호
