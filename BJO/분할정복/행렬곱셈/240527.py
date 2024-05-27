5e8e819eb554bcf9fea625e09f1ac143bece44b0
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-05-27 23:06:31 +0900

    240527

--- BJO/분할정복/행렬곱셈/240527.py
+++ BJO/분할정복/행렬곱셈/240527.py
@@ -1,14 +1,13 @@
 import sys
 input = sys.stdin.readline
-
 N,M = map(int,input().split())
 A = [list(map(int,input().split())) for _ in range(N)]
-
 
 M,K = map(int,input().split())
 B = [list(map(int,input().split())) for _ in range(M)]
 
-C = [[0 for _ in range(K)] for _ in range(N)]
+C = [[0] * K for _ in range(N)]
+
 
 for n in range(N):
     for k in range(K):
