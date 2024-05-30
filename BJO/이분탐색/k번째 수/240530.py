d29b28f21faa8a66cd3607d1c7a1abab889e3c0a
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-05-30 23:31:31 +0900

    240530

---
+++ BJO/이분탐색/k번째 수/240530.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+k = int(input())
+
+lt, rt = 0, k
+
+while lt<=rt:
+    mid = (lt + rt) // 2
+    count = 0
+    for i in range(1, n+1):
+        count += min(mid // i, n)
+    
+    if count >= k:
+        rt = mid - 1
+        answer = mid
+    else:
+        lt = mid + 1
+
+print(answer)
