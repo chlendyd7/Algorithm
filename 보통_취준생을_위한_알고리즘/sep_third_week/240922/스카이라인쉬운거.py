31718889f01aaf4db386a762aa03d91db65d2e6f
Doo Young <88640035+chlendyd7@users.noreply.github.com>
2024-09-25 15:34:12 +0900

    240925

--- 보통_취준생을_위한_알고리즘/sep_third_week/240922/스카이라인쉬운거.py
+++ 보통_취준생을_위한_알고리즘/sep_third_week/240922/스카이라인쉬운거.py
@@ -5,17 +5,12 @@ def solution(skylines):
         height = p
         while stk[-1] > p:
             if stk[-1] != height:
-                answer +=1
+                answer += 1
                 height = stk[-1]
             stk.pop()
         stk.append(p)
 
-
-skylines = []
 n = int(input())
-for i in range(n):
-    skylines.append(int(input().split()[1]))
+skylines = [list(map(int,input().split()[1])) for _ in range(n)]
 skylines.append(0)
 answer = 0
-solution(skylines)
-print(answer)
