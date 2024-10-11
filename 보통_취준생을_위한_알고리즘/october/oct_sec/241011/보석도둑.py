a1e86045130c2ddb17d81909eb2f031dafa31a5d
Doo Young <88640035+chlendyd7@users.noreply.github.com>
2024-10-11 14:26:15 +0900

    201011

--- 보통_취준생을_위한_알고리즘/october/oct_sec/241011/보석도둑.py
+++ 보통_취준생을_위한_알고리즘/october/oct_sec/241011/보석도둑.py
@@ -1,17 +1,14 @@
-import sys
 import heapq
 
-N, K = map(int, sys.stdin.readline().split())
-jew = []
-for _ in range(N):
-    heapq.heappush(jew, list(map(int, sys.stdin.readline().split())))
-bags = []
-for _ in range(K):
-    bags.append(int(sys.stdin.readline()))
-bags.sort()
 
-answer = 0
+n,k = map(int, input().split())
+jew = []
+for _ in range(n):
+    heapq.heappush(jew, list(map(int, input().split())))
+bags = [int(input()) for _ in range(k)]
+bags.sort()
 tmp_jew = []
+answer = 0
 for bag in bags:
     while jew and bag >= jew[0][0]:
         heapq.heappush(tmp_jew, -heapq.heappop(jew)[1])
