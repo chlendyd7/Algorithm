fe0f8b30e72ca5d8d1cf3042759c15d93dc5d0dd
Doo Young <88640035+chlendyd7@users.noreply.github.com>
2024-10-16 13:48:28 +0900

    241016

--- TODO/알고리즘/그리디/센서.py
+++ TODO/알고리즘/그리디/센서.py
@@ -12,3 +12,15 @@ for _ in range(K-1):
     dist.pop(0)
 
 print(sum(dist))
+
+
+'''
+N = int(input())
+K = int(input())
+sensor = list(map(int, input().split()))
+sensor.sort()
+
+dist = [sensor[i] - sensor[i-1] for i in range(1, N)]
+dist.sort(reverse=True)
+print(sum(dist[K-1:]))
+'''
