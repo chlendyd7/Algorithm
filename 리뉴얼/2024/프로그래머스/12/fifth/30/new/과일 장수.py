f11be25da701eb5b8f6e0e9199d2b66f5a3134cb
chlendyd7 <88640035+chlendyd7@users.noreply.github.com>
2024-12-30 22:18:01 +0900

    241230

--- 리뉴얼/2024/프로그래머스/12/fifth/30/new/과일 장수.py
+++ 리뉴얼/2024/프로그래머스/12/fifth/30/new/과일 장수.py
@@ -5,16 +5,15 @@ def solution(k, m, score):
     counter_lst = sorted(counter.items(), key=lambda x: -x[0])
     
     answer = 0
-    current_box = []  # 현재 상자에 담긴 사과 점수
+    current_box = []
 
     for price, num in counter_lst:
-        while num > 0:  # 현재 점수 그룹의 사과를 처리
+        while num > 0:
             current_box.append(price)
             num -= 1
             
-            # 상자가 꽉 찼을 때 처리
             if len(current_box) == m:
                 answer += min(current_box) * m
-                current_box = []  # 상자를 초기화
+                current_box = []
 
     return answer
