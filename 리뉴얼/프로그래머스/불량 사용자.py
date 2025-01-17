948f4cba04aaa2b395b6e9c0fb3fb5e8cd05eefb
Doo Young <88640035+chlendyd7@users.noreply.github.com>
2025-01-17 11:55:33 +0900

    250117

--- 리뉴얼/프로그래머스/불량 사용자.py
+++ 리뉴얼/프로그래머스/불량 사용자.py
@@ -1,31 +1,26 @@
 from itertools import permutations
 
-def is_match(banned_id, user_id):
-    # banned_id와 user_id의 길이가 다르면 매칭 불가
-    if len(banned_id) != len(user_id):
-        return False
-    
-    # banned_id의 각 문자와 user_id 비교
-    for b_char, u_char in zip(banned_id, user_id):
-        if b_char != '*' and b_char != u_char:
+
+def is_match(users, banned_id):
+    for i in range(len(banned_id)):
+        if len(users[i]) != len(banned_id[i]):
             return False
-    
+
+        for j in range(len(users[i])):
+            if banned_id[i][j] == '*':
+                continue
+            if banned_id[i][j] != users[i][j]:
+                return False
     return True
 
 def solution(user_id, banned_id):
+    user_permutation = list(permutations(user_id, len(banned_id)))
+    ban_set = []
 
-    # 가능한 모든 조합 생성
-    banned_permutations = list(permutations(user_id, len(banned_id)))
-    print(banned_permutations)
-    valid_combinations = set()
+    for users in user_permutation:
+        if is_match(users, banned_id):
+            users = set(users)
+            if users not in ban_set:
+                ban_set.append(users)
     
-    for perm in banned_permutations:
-        match = True
-        for b_id, u_id in zip(banned_id, perm):
-            if not is_match(b_id, u_id):
-                match = False
-                break
-        if match:
-            valid_combinations.add(tuple(sorted(perm)))
-    
-    return len(valid_combinations)
+    return len(ban_set)
