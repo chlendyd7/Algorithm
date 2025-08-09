2b0c577154cc7d3fa354d982119e16b91a97885a
chlendyd7 <chlendyd7@naver.com>
2025-08-09 13:31:56 +0900

    250809

---
+++ SWEA/9386. 연속한 1의 개수.py
@@ -0,0 +1,17 @@
+def solution():
+    N = int(input())
+    nums = 
+    
+    return
+
+
+T = int(input())
+for t in range(1, T+1):
+    print(f'#{t} {solution()}')
+
+
+'''
+섭섭하데 안챙겨줘서
+배려가 배려가 아니였대
+
+'''
