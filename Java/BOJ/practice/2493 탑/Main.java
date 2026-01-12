81e563bf169acc7ed75399b6275a89750f8ac4f9
Doo Young <chlendyd7@naver.com>
2026-01-12 13:23:21 +0900

    260112

--- Java/BOJ/practice/2493 탑/Main.java
+++ Java/BOJ/practice/2493 탑/Main.java
@@ -4,6 +4,35 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Integer N = Integer.parseInt(br.readLine());
+        String[] input = br.readLine().split(" ");
+        int[] tops = Arrays.stream(input)
+                   .mapToInt(Integer::parseInt)
+                   .toArray();
+        
+        int[] result = new int[N];
+        Deque<int[]> stack = new ArrayDeque<>();
 
+        for (int i=0; i<N; i++) {
+            int nowTop = tops[i];
+
+            while (!stack.isEmpty() && stack.peek()[0] < nowTop) {
+                stack.pop();
+            }
+
+            if(!stack.isEmpty()){
+                result[i] = stack.peek()[1] + 1;
+            } else {
+                result[i] = 0;
+            }
+
+            stack.push(new int[]{nowTop, i});
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<N; i++) {
+            sb.append(result[i]).append(" ");
+        }
+        System.out.println(sb.toString().trim());
     }
 }
--- Java/BOJ/practice/2792 보석상자/Main.java
+++ Java/BOJ/practice/2792 보석상자/Main.java
@@ -4,6 +4,36 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int N = Integer.parseInt(input[0]);
+        int M = Integer.parseInt(input[1]);
 
+        List<Integer> gems = new ArrayList<>();
+        int left = 1;
+        int right = 0;
+
+        for (int i=0;i<M;i++) {
+            int currentGem = Integer.parseInt(br.readLine());
+            gems.add(currentGem);
+
+            right = Math.max(right, currentGem);
+        }
+        int zilous = right;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            int totalCount = 0;
+            for (int i=0; i<M;i++) {
+                totalCount += (gems.get(i) / mid);
+                if (gems.get(i) % zilous != 0) totalCount += 1;
+            }
+
+            if (totalCount <= N) {
+                zilous = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        System.out.println(zilous);
     }
 }
