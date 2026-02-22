9eb001318c5338197e3374fdc1d4c1a6ee5b794b
chlendyd7 <chlendyd7@naver.com>
2026-02-23 07:58:11 +0900

    260223

--- Programmers/43165 타겟 넘버/Main.java
+++ Programmers/43165 타겟 넘버/Main.java
@@ -1,9 +1,22 @@
-import java.io.*;
-import java.util.*;
-
-public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+class Solution {
+    int answer = 0;
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, 0, target, 0);
+        return answer;
+    }
+    
+    private void dfs(int[] numbers, int i, int target, int result) {
+        if (i==numbers.length) {
+            if (result == target) {
+                answer ++;
+            }
+            return;
+        }
+        
+        dfs(numbers, i+1, target, result + numbers[i]);
+        dfs(numbers, i+1, target, result - numbers[i]);
 
     }
+    
+    
 }
